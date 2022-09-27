0f91cc70023cc3374b9765c956653be764b1da00
avkucher <99530288+avkucher@users.noreply.github.com>
2022-09-27 14:56:21 +0300

    Диплом

---
+++ source/core/urls.py
@@ -0,0 +1,9 @@
+from core.views import LoginView, ProfileView, SignupView, UpdatePasswordView
+from django.urls import path
+
+urlpatterns = [
+    path('signup', SignupView.as_view(), name='signup'),
+    path('login', LoginView.as_view(), name='login'),
+    path('profile', ProfileView.as_view(), name='profile'),
+    path('update_password', UpdatePasswordView.as_view(), name='update-password'),
+]
