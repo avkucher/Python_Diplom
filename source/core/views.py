3e8bc156fadbe2915c4e830742717deaff05ec91
avkucher <99530288+avkucher@users.noreply.github.com>
2022-10-04 22:47:50 +0300

    Диплом

--- source/core/views.py
+++ source/core/views.py
@@ -17,7 +17,7 @@ class LoginView(generics.GenericAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         login(request=request, user=serializer.save())
-        return Response(serializer.data)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class ProfileView(generics.RetrieveUpdateDestroyAPIView):
